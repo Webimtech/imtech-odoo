2e86fbb17a26733c1f47909b305b3c238f27fc89
Webimtech <66035639+Webimtech@users.noreply.github.com>
2020-06-10 12:26:36 +0200

    Delete __init__.py

--- icecatmodule/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-<<<<<<< HEAD:icecatmodule/models/__init__.py
-from . import models
-=======
-from . import models
->>>>>>> 2339dd14ce8725f1c1ff0d352d56c488f1b13489:icemodule/models/__init__.py
